2ec0f3e0a701d6e2630cde4c21058fbfa4f58f29
Anton Babko <anton.babko@ukma.edu.ua>
2022-10-27 17:03:03 +0300

    Initial Commit

---
+++ O-Tic/O_TicApp.swift
@@ -0,0 +1,17 @@
+//
+//  O_TicApp.swift
+//  O-Tic
+//
+//  Created by admin on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct O_TicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
